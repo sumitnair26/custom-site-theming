09a5a24d6c552b004cf60583e607ba058b9513aa
sumitnair26 <sumit.nair26@gmail.com>
2023-07-18 17:48:29 +0530

    On load of page take values from local storage and use same setting or default CSS

--- settings.js
+++ settings.js
@@ -12,6 +12,27 @@ const doc = document.documentElement;
 
 let isAudioPlayable;
 
+//settings with default
+
+const settings = [
+    {
+        key: "sound",
+        default: "false"
+    },
+    {
+        key: "motion",
+        default: "true"
+    },
+    {
+        key: "theme",
+        default: "system"
+    },
+    {
+        key: "customColor",
+        default: "accent2"
+    }
+]
+
 // functions
 function updateSiteUi({name, value}){
     //console.log({ name, value });
@@ -19,6 +40,20 @@ function updateSiteUi({name, value}){
         return doc.style.setProperty("--customColor", `var(--${value})`);
     }
     return (doc.dataset[name]=value);
+}
+function updateSettingUi({name, value}){
+    // boolean Toggles
+    if(value ==="true" || value === "false") {
+        const checkbox = document.querySelector(`[name="${name}"]`);
+
+        return (checkbox.checked = value === "true" ? true : false);
+
+    }
+
+    //all remaining radios
+
+    const radio = document.querySelector(`#${value}`);
+    return (radio.checked = true);
 }
 
 function playAudio(type) {
@@ -30,6 +65,19 @@ function playAudio(type) {
 }
 
 // event listners 
+
+window.addEventListener("DOMContentLoaded", () =>{
+    settings.forEach((setting)=> {
+        const value = localStorage.getItem(setting.key) ?? setting.default;
+       //console.log({ name: setting.key, value})
+       updateSiteUi({name:setting.key, value});
+       updateSettingUi({name:setting.key, value});
+       if(setting.key === "sound"){
+        isAudioPlayable = value === "true" ? true : false;
+       }
+    })
+})
+
 toggles.forEach((toggle) =>{ 
     toggle.addEventListener("change",(e)=> {
         const { name, checked } = e.target;
