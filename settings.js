9352442e69ff338e4aea330d5b249f3a95782435
sumitnair26 <sumit.nair26@gmail.com>
2023-07-18 13:48:54 +0530

    Adding Setings.js to manage toggles

---
+++ settings.js
@@ -0,0 +1,20 @@
+console.log('inside settings');
+
+//selectors
+const toggles = document.querySelectorAll(".settings [type='checkbox']"); 
+
+const doc = document.documentElement;
+
+// functions
+function updateSiteUi({name, value}){
+    //console.log({ name, value });
+    return (doc.dataset[name]=value);
+}
+
+// event listners 
+toggles.forEach((toggle) =>{ 
+    toggle.addEventListener("change",(e)=> {
+        const { name, checked } = e.target;
+        updateSiteUi({name, value: checked})
+    })
+})
