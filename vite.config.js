59437a10a5317b493e8668762453876687d8d561
sumitnair26 <sumit.nair26@gmail.com>
2024-09-01 23:41:47 +0530

    removed react plugin from vite config

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,9 @@
 import { defineConfig } from 'vite';
-import react from "@vitejs/plugin-react";
 
 export default defineConfig({
    server: {
         host: true,
         port: 80
    },
-   plugins: [react()],
    base: "/custom-site-theming/"
 })
