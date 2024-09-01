47d2f03549a95afe4dea28544763d3a59b4a8e8c
sumitnair26 <sumit.nair26@gmail.com>
2024-09-01 23:31:58 +0530

    changes to deploy in github pages

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,11 @@
-import { defineConfig } from 'vite'
+import { defineConfig } from 'vite';
+import react from "@vitejs/plugin-react";
 
 export default defineConfig({
    server: {
         host: true,
         port: 80
-   }
+   },
+   plugins: [react()],
+   base: "/custom-site-theming/"
 })
